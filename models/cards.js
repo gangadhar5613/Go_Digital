13b1a4c73d8fb4e055d0c6b80fba0b9ed6d86f47
Shadab Ali <shadabali604@gmail.com>
2020-12-03 18:53:42 +0530

    added: schema for card and articles

---
+++ models/cards.js
@@ -0,0 +1,49 @@
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+
+// User Data
+
+let Card = mongoose.model(
+  "Card",
+  new Schema({
+    name: {
+      type: String,
+      require: true,
+      uppercase: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      lowercase: true,
+    },
+    avatar: {
+      type: Buffer,
+    },
+    number: {
+      type: Number,
+      minlength: 10,
+      maxlength: 12,
+      require: true,
+      unique: true,
+    },
+    address: {
+      type: String,
+      minlength: 15,
+      require: true,
+    },
+    facebook: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+  })
+);
+
+module.exports = Card;
